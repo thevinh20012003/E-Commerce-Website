62c058cea8ef00b771460b15c4871ea2620499ca
VinhTThe <VinhTThe176288@fpt.edu.vn>
2025-07-21 19:53:12 +0700

    Integrating Angular and Spring boot for Full stack app

--- backend/src/main/java/com/vinhtt/ecommerce/dao/ProductCategoryRepository.java
+++ backend/src/main/java/com/vinhtt/ecommerce/dao/ProductCategoryRepository.java
@@ -3,6 +3,7 @@ package com.vinhtt.ecommerce.dao;
 import com.vinhtt.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // tùy chỉnh tên Json và path của endpoints
 @RepositoryRestResource(
@@ -12,5 +13,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
         // product-category
         path = "product-category"
 )
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- backend/src/main/java/com/vinhtt/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/com/vinhtt/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.vinhtt.ecommerce.dao;
 
 import com.vinhtt.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // Allow CORS for Angular frontend
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
