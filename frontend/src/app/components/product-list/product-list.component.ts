96c071323c64a09ef34dc97263ed7ed52ef56ffe
VinhTThe <VinhTThe176288@fpt.edu.vn>
2025-07-29 14:26:02 +0700

    feat(ui): implement detail view for selected product

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,64 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
-  // templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[] = [];
-
+  products: Product[] = []
   currentCategoryId: number = 1;
-
   searchMode: boolean = false;
 
   constructor(private productService: ProductService,
-    private router: ActivatedRoute
-  ) { }
+              private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.router.paramMap.subscribe(() => {
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
-    })
+    });
   }
 
   listProducts() {
-    this.searchMode = this.router.snapshot.paramMap.has('keyword');
-    if(this.searchMode){
+
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
       this.handleSearchProducts();
-    }else{
+    }
+    else {
       this.handleListProducts();
     }
+
   }
+
   handleSearchProducts() {
-    const theKeyword: string = this.router.snapshot.paramMap.get('keyword')!;
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
     this.productService.searchProducts(theKeyword).subscribe(
       data => {
         this.products = data;
       }
     )
   }
+
   handleListProducts() {
-    // check if id parameter is available
-    const hasCategoryId: boolean = this.router.snapshot.paramMap.has('id');
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
     if (hasCategoryId) {
-      // get the "id" param string. convert string to a number using "+" symbol
-      this.currentCategoryId = +this.router.snapshot.paramMap.get('id')!;
-    } else {
-      // not category id available... default to category id 1
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
+
+    // now get the products for the given category id
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
-    )
+    )    
   }
-
 }
