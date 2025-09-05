35b232ea55403f95216873d8fc17425091fd47cd
VinhTThe <VinhTThe176288@fpt.edu.vn>
2025-09-05 12:31:18 +0700

    feat(HTTps): Secure Https Backend and Fronted

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: "0oauvt97epNF9qjr2697",   
     issuer: "https://integrator-8319255.okta.com/oauth2/default", 
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 };
